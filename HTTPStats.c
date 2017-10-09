dd0027b2c790d57b129d6afbd0d94abce9da7073
matheus <hm.matheus@gmail.com>
2017-10-09 23:30:14 +0100

    Increase number of potential on-the-fly tests to 16 (this library is blocking and not thread-safe). The idea is that we can store the result of up to 16 tests before returning all the metrics.
    Older tests are overwritten if their data is not retrieved and reset.

--- HTTPStats.c
+++ HTTPStats.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define MAX_ITEMS 1024
+#define MAX_TESTS 16
 
 typedef struct
 {
@@ -16,7 +17,7 @@ typedef struct
 typedef struct
 {
     void* implementationHandle;
-    HTTPStatsResult_ workArena;
+    HTTPStatsResult_ workArena[16];
     HTTPStatsResult nrCurrentResults;
 } HTTPStatsContext;
 
@@ -57,17 +58,19 @@ void HTTPStatsMetrics(HTTPStats* handle, HTTPStatsResult result,
 {
     if (!handle)
         return;
+    if (result < 0 || result >= MAX_TESTS)
+        return;
 
     HTTPStatsContext* ctx = (HTTPStatsContext*)handle;
     // For each one of the requested metrics, compute the final result
     for (unsigned i = HTTPSTATS_MAX_METRICS >> 1; i != 0; i >>=1 )
     {
-        if (!(i & ctx->workArena.requestedMetrics))
+        if (!(i & ctx->workArena[result].requestedMetrics))
             continue;
 
-        qsort(ctx->workArena.results[i], ctx->workArena.measurements[i], sizeof(double),
+        qsort(ctx->workArena[result].results[i], ctx->workArena[result].measurements[i], sizeof(double),
               cmpfunc);
-        computedValues[i] = ctx->workArena.results[i][ctx->workArena.measurements[i] / 2];
+        computedValues[i] = ctx->workArena[result].results[i][ctx->workArena[result].measurements[i] / 2];
     }
 }
 
@@ -80,7 +83,9 @@ HTTPStatsResult HTTPGetTest(HTTPStats* handle, HTTPTestDescription desc)
 
     double* tmpStorage;
     tmpStorage = (double*)malloc(sizeof(double) * HTTPSTATS_MAX_METRICS);
-    ctx->workArena.requestedMetrics = desc.metricsMask;
+    HTTPStatsResult r = ctx->nrCurrentResults;
+    ctx->nrCurrentResults = (ctx->nrCurrentResults + 1) % MAX_TESTS;
+    ctx->workArena[r].requestedMetrics = desc.metricsMask;
 
     for (int it = 0; it < desc.iterations; it++)
     {
@@ -94,18 +99,18 @@ HTTPStatsResult HTTPGetTest(HTTPStats* handle, HTTPTestDescription desc)
             if (!(i & desc.metricsMask))
                 continue;
 
-            int idx = ctx->workArena.measurements[i];
-            ctx->workArena.results[i][idx] = tmpStorage[i];
+            int idx = ctx->workArena[r].measurements[i];
+            ctx->workArena[r].results[i][idx] = tmpStorage[i];
 
             // Update index
             idx += 1;
             if (idx > MAX_ITEMS)
                 idx = MAX_ITEMS;
-            ctx->workArena.measurements[i] = idx;
+            ctx->workArena[r].measurements[i] = idx;
         }
     }
 
     free(tmpStorage);
 
-    return 0;
+    return r;
 }
